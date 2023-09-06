61fd057b1ba33c6f6a64123c8b16a1cd6ec9a57b
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-06 12:33:06 -0500

    feat: :construction: add controllers

--- Api/Controllers/AreaRepository.cs
+++ Api/Controllers/AreaRepository.cs
@@ -1,10 +1,94 @@
-
-using Application.Repositories.Generics;
+using Api.Controllers;
+using Api.Dtos;
+using AutoMapper;
+using Api.Helpers.Paginator;
 using Domain.Entities;
 using Domain.Interface;
-using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Interface.Pagination;
 
-namespace Application.Repositories;
-public class AreaRepository : GenericRepositoryWithIntId<Area>, IAreaRepository{
-    public AreaRepository(ApiContext context) : base(context){}
+namespace ApiIncidencias.Controllers;
+[ApiVersion("1.0")]
+public class AreaController : BaseApiController{
+    private readonly IUnitOfWork _UnitOfWork;
+    private readonly IMapper _Mapper;
+
+    public AreaController (IUnitOfWork unitOfWork,IMapper mapper){
+        _UnitOfWork = unitOfWork;
+        _Mapper = mapper;
+    }
+
+    [HttpGet]
+    //[Authorize]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IEnumerable<AreaDto>> Get(){
+       var records = await _UnitOfWork.Areas.Find();
+       return _Mapper.Map<List<AreaDto>>(records);
+    }
+
+    [HttpGet("{id}")]
+    //[Authorize]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<AreaDto>> Get(int id){
+       var record = await _UnitOfWork.Areas.FindByIntId(id);
+       if (record == null){
+           return NotFound();
+       }
+       return _Mapper.Map<AreaDto>(record);
+    }
+
+    [HttpGet]
+    [MapToApiVersion("1.1")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Pager<AreaDto>>> Get11([FromQuery] PageParam param){
+       IPager<Area> pager = await _UnitOfWork.Areas.Find(param);
+       pager.Records = (IEnumerable<Area>)_Mapper.Map<List<AreaDto>>(pager.Records);        
+       return CreatedAtAction("Area",pager);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<AreaDto>> Post(AreaDto recordDto){
+       var record = _Mapper.Map<Area>(recordDto);
+       _UnitOfWork.Areas.Add(record);
+       await _UnitOfWork.SaveChanges();
+       if (record == null){
+           return BadRequest();
+       }
+       return CreatedAtAction(nameof(Post),new {id= record.IdPk, recordDto});
+    }
+
+    [HttpPut]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<AreaDto>> Put([FromBody]AreaDto? recordDto){
+       if(recordDto == null)
+           return NotFound();
+       var record = _Mapper.Map<Area>(recordDto);
+       _UnitOfWork.Areas.Update(record);
+       await _UnitOfWork.SaveChanges();
+       return recordDto;
+    }
+
+    [HttpDelete("{id}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id){
+       var record = await _UnitOfWork.Areas.FindByIntId(id);
+       if(record == null){
+           return NotFound();
+       }
+       _UnitOfWork.Areas.Remove(record);
+       await _UnitOfWork.SaveChanges();
+       return NoContent();
+    }
 }
--- Api/Controllers/ContactTypeRepository.cs
+++ Api/Controllers/ContactTypeRepository.cs
@@ -1,9 +1,94 @@
-using Application.Repositories.Generics;
+using Api.Controllers;
+using Api.Dtos;
+using AutoMapper;
+using Api.Helpers.Paginator;
 using Domain.Entities;
 using Domain.Interface;
-using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Interface.Pagination;
 
-namespace Application.Repositories;
-public class ContactTypeRepository : GenericRepositoryWithIntId<ContactType>, IContactTypeRepository{
-    public ContactTypeRepository(ApiContext context) : base(context){}
+namespace ApiIncidencias.Controllers;
+[ApiVersion("1.0")]
+public class ContactTypeController : BaseApiController{
+    private readonly IUnitOfWork _UnitOfWork;
+    private readonly IMapper _Mapper;
+
+    public ContactTypeController (IUnitOfWork unitOfWork,IMapper mapper){
+        _UnitOfWork = unitOfWork;
+        _Mapper = mapper;
+    }
+
+    [HttpGet]
+    //[Authorize]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IEnumerable<ContactTypeDto>> Get(){
+       var records = await _UnitOfWork.ContactTypes.Find();
+       return _Mapper.Map<List<ContactTypeDto>>(records);
+    }
+
+    [HttpGet("{id}")]
+    //[Authorize]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ContactTypeDto>> Get(int id){
+       var record = await _UnitOfWork.ContactTypes.FindByIntId(id);
+       if (record == null){
+           return NotFound();
+       }
+       return _Mapper.Map<ContactTypeDto>(record);
+    }
+
+    [HttpGet]
+    [MapToApiVersion("1.1")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Pager<ContactTypeDto>>> Get11([FromQuery] PageParam param){
+       IPager<ContactType> pager = await _UnitOfWork.ContactTypes.Find(param);
+       pager.Records = (IEnumerable<ContactType>)_Mapper.Map<List<ContactTypeDto>>(pager.Records);        
+       return CreatedAtAction("ContactType",pager);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ContactTypeDto>> Post(ContactTypeDto recordDto){
+       var record = _Mapper.Map<ContactType>(recordDto);
+       _UnitOfWork.ContactTypes.Add(record);
+       await _UnitOfWork.SaveChanges();
+       if (record == null){
+           return BadRequest();
+       }
+       return CreatedAtAction(nameof(Post),new {id= record.IdPk, recordDto});
+    }
+
+    [HttpPut]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ContactTypeDto>> Put([FromBody]ContactTypeDto? recordDto){
+       if(recordDto == null)
+           return NotFound();
+       var record = _Mapper.Map<ContactType>(recordDto);
+       _UnitOfWork.ContactTypes.Update(record);
+       await _UnitOfWork.SaveChanges();
+       return recordDto;
+    }
+
+    [HttpDelete("{id}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id){
+       var record = await _UnitOfWork.ContactTypes.FindByIntId(id);
+       if(record == null){
+           return NotFound();
+       }
+       _UnitOfWork.ContactTypes.Remove(record);
+       await _UnitOfWork.SaveChanges();
+       return NoContent();
+    }
 }
